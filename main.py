0de3769130cb3e8f8ce220cc3c1d9ac693163d97
Elizabeth W <ellewoo12@gmail.com>
2020-11-24 22:34:52 +0000

    neater comments

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import random #creates random generator
 import io #Helps with file making (writing, reading, open)
 
+#First function: Generates a random spidroin sequence with 2 smaller subsets (total of 300 nuceotides)
 def dnaSequence():
-  #nucleotide list A T C G
+  #creates nucleotide list A T C G
   nucList = list("ATCG")
 
   #empty lists of the first 2 sub sets
@@ -11,36 +12,48 @@ def dnaSequence():
   subrepeatB = ""
 
   #first subset to make a sequence for the first subset
-  count1 = random.randrange(1,76) #picks a random number/ amount for how many nucleotides there should be
+
+  #random.randrange(1,76) picks a random number/ amount for how many nucleotides there should be
+  #the for loop adds the random combination of nucleotides to the empty list
+  count1 = random.randrange(1,76) 
   for i in range(0,count1):
-    subrepeatA += random.choice(nucList) #adds the random combination of nucleotides to the empty list
-
-  count2 = 150-count1 #number for amount of nucleotides in second subset
+    subrepeatA += random.choice(nucList) 
+  
+  #number for amount of nucleotides in second subset
+  #for loop adds the random combination to the string
+  count2 = 150-count1 
   for i in range(0,count2):
-    subrepeatB += random.choice(nucList)#adds the random combination to the string
+    subrepeatB += random.choice(nucList)
 
-  largeSubset = subrepeatA + subrepeatB + subrepeatA + subrepeatB #large subset adds the 2 sets together twice to get total of 300
+  #large subset adds the 2 sets together twice to get total of 300
+  largeSubset = subrepeatA + subrepeatB + subrepeatA + subrepeatB 
 
   return(largeSubset) 
 
 #Putting the function into a variable
 spidroin = dnaSequence()
 
-# Second function
+
+#Second function to make the sequence created in the first function into a fasta file.
 
 #Ask the user what they want the file name and heading to be for the file
 filename = input("File name is: ")
 fileheader = input("file header is: ")
 
-#Second function to make the sequence created in the first function into a fasta file.
+#try block helps avoid errors
+#the with open(filename.fasta, "w") includes title of the file inputted and set it as a handle (spidroinFile_handle)
+#.write(">"+ fileheader + "\n") writes the fileheader and \n helps make the sequence on the next line
+#.write(spidroin) adds the sequence to the file
+#return the handle
 def SpidroinFile(filename, fileheader):
-  try: #try block helps avoid errors
-    with open(filename + ".fasta", "w") as spidroinFile_handle:#includes title of the file inputted and set it as a handle (spidroinFile_handle)
-      spidroinFile_handle.write(">"+ fileheader + "\n") #this writes the fileheader and \n helps make the sequence on the next line
-      spidroinFile_handle.write(spidroin) #adding the sequence to the file
+  try: 
+    with open(filename + ".fasta", "w") as spidroinFile_handle:
+      spidroinFile_handle.write(">"+ fileheader + "\n") 
+      spidroinFile_handle.write(spidroin) 
   except IOError as err:
     print("there's an error")
-  return(spidroinFile_handle) #returning the handle
+  return(spidroinFile_handle) 
 
-SpidroinFile(filename, fileheader) #calling the function so that it makes the file.
+#calling the function so that it makes the file.
+SpidroinFile(filename, fileheader) 
 
